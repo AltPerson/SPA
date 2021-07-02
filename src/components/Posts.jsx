22098286f0c34230c5c9d625d01e2f7173b367ab
AltPerson <easythanitseems@gmail.com>
2021-07-02 18:46:22 +0300

    Change Imgs import

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,20 +1,23 @@
 import Post from "./Post";
+import carImg from "../img/car.jpg";
+import manImg from "../img/man.jpg";
+import peopleImg from "../img/people.jpg";
 
 function Posts() {
   return (
     <div className="posts-inner">
       <Post
-        src="../img/car.jpg"
+        src={carImg}
         title="Car at low price"
         description="The stranger sells a car at very low price only 2000$"
       />
       <Post
-        src="../img/man.jpg"
+        src={manImg}
         title="A man who reached a sky"
         description="The stranger reached a sky in a very interesting way"
       />
       <Post
-        src="../img/people.jpg"
+        src={peopleImg}
         title="Why Don't Young People Read the News?"
         description="Kids Are Too Busy With Facebook and Texting, Author Says"
       />
